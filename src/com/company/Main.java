4808140ddde8be23c7da315e39eb672138de4121
Inanc Yucel <inancyucel@gmail.com>
2021-10-12 18:33:18 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,107 @@
+package com.company;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void islemleri_bastir(){
+
+        System.out.println("0 - İşlemleri Görüntüle...");
+        System.out.println("1 - Bir sonraki şehre git...");
+        System.out.println("2 - Bir önceki şehre git...");
+        System.out.println("3 - Uygulamadan çık...");
+
+
+
+    }
+    public static void sehirleri_turla(LinkedList<String> sehirler){
+
+        ListIterator<String> iterator = sehirler.listIterator();
+
+        int islem;
+
+        islemleri_bastir();
+
+        Scanner scanner = new Scanner(System.in);
+
+        if(!iterator.hasNext()){
+            System.out.println("Herhangibir Şehir bulunmuyor");
+        }
+
+        boolean cikis = false;
+        boolean ileri = true;
+
+        while(!cikis) {
+
+            System.out.println("Bir işlem seçiniz");
+
+            islem = scanner.nextInt();
+
+
+            switch (islem){
+
+                case 0:
+                islemleri_bastir();
+                break;
+
+                case 1:
+
+                    if (!ileri){
+                        if(iterator.hasNext()){
+                            iterator.next();
+                        }
+                        ileri=true;
+                    }
+                if(iterator.hasNext()){
+
+                    System.out.println("Şehre gidiliyor: " + iterator.next());
+
+                }
+                else
+                    System.out.println("Gidilecek şehir kalmadı");
+
+                break;
+
+                case 2:
+
+                    if (ileri){
+                        if(iterator.hasPrevious()){
+                            iterator.previous();
+                        }
+                        ileri=false;
+                    }
+
+                    if(iterator.hasPrevious()){
+
+                        System.out.println("Şehre gidiliyor: " + iterator.previous());
+
+                    }
+                    else
+                        System.out.println("Şehir turu başladı");
+                    break;
+
+                case 3:
+                    cikis = true;
+                    System.out.println("Uygulamadan çıkılıyor");
+                    break;
+
+
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        LinkedList<String> sehirler = new LinkedList<String>();
+
+        sehirler.add("Ankara");
+        sehirler.add("Eskişehir");
+        sehirler.add("Afyon");
+
+        sehirleri_turla(sehirler);
+
+
+    }
+}
